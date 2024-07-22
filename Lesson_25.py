ebd3e75c225e4f327a6bebf7c0b4ebc92b27c7a2
Alexandr Dotu <alexdotu@gmail.com>
2024-07-22 22:02:30 +0200

    create file lesson_25

---
+++ Lesson_25.py
@@ -0,0 +1,6 @@
+number1 = float(input('write first number: '))
+number2 = float(input('write second number: '))
+number3 = float(input('write third number: '))
+summa = number1 + number2 + number3
+multiplication = number1 * number2 * number3
+print(summa, '\n', multiplication)
